7cb68838d7d7f2eeecfc8633b47155dd5f61de8e
Y0ooSibbi <alpesh57678@gmail.com>
2022-11-26 16:38:10 +0530

    Added features to send mail

---
+++ config/mailConfig.js
@@ -0,0 +1,8 @@
+mailConfig=[{
+    uuid:"e3f588b6-6d76-11ed-a1eb-0242ac120002",
+    user:"yorsibbu@gmail.com",
+    pass:"bgayhmpoefcxwmpa"
+}]
+
+
+module.exports = mailConfig
--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const nodemailer = require("nodemailer");
+const mailConfig = require('../config/mailConfig');
 
 
 
@@ -8,34 +9,38 @@ const nodemailer = require("nodemailer");
 // send mail
 router.post("/sendMail",  (req, res) => {
 
-    const { email } = req.body;
-  
-
+    const { uuid, to,subjectOfMail,dataOfMail } = req.body;
     try {
+        mailConfig.forEach(element => {
+            if(element.uuid==uuid){
+                const transporter = nodemailer.createTransport({
+                    service: "gmail",
+                    auth: {
+                        user: element.user,
+                        pass: element.pass
+                    }
+                });
+        
+                const mailOptions = {
+                    from: element.user,
+                    to: to,
+                    subject: subjectOfMail,
+                    html: dataOfMail
+                };
+        
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log("Error" + error)
+                    } else {
+                        console.log("Email sent:" + info.response);
+                        res.status(201).json({status:201,info})
+                    }
+                })
 
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD
             }
         });
 
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: "Sending Email With React And Nodejs",
-            html: '<h1>Congratulation</h1> <h1> You successfully sent Email </h2>'
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log("Error" + error)
-            } else {
-                console.log("Email sent:" + info.response);
-                res.status(201).json({status:201,info})
-            }
-        })
+        
 
     } catch (error) {
         console.log("Error" + error);
